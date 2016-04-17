5733af97d07e4ec4cd51f8606ffedbc28ef59e37
shengjianfei <376337002@qq.com>
2016-04-17 18:41:40 +0800

    Update test.js

--- WeChat-Lucky-Money/js/test.js
+++ WeChat-Lucky-Money/js/test.js
@@ -208,8 +208,8 @@ function load (){
 	}
 	function touchEnd(event) {
 		$("#play-btn").html("X轴移动大小：" + (endX - startX)+"<br/>"+"Y轴移动大小：" + (endY -startY));
-		if((endX - startX)>50){
-			alert((endX - startX));
+		if((startY - endY)>50){
+			alert((startY - endY));
 			var money = changeTwoDecimal(MoneyDistribution());
 			if (EnvelopeMove == 0) {
 				EnvelopeMove = 1;
